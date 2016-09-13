c9fd4b3cd20a54828d3a9b6386430bf01ff7b451
vineetpalan <vineetpalan@gmail.com>
2016-09-13 12:28:54 -0400

    Create STRHH.cpp

---
+++ STRHH.cpp
@@ -0,0 +1,25 @@
+/*STRHH-Half of the Half -Spoj*/
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() 
+{
+	int testCases=0;
+	cin>>testCases;
+	char inputSequence[201];
+	
+	for (int i=0;i< testCases; i++)
+	{
+		cin>>inputSequence;
+		int len = strlen(inputSequence)/2;
+		for (int j=0;j<len;j+=2)
+		{
+			cout<<inputSequence[j];
+		}
+		cout<<endl;
+	}
+	return 0;
+}
